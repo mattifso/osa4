6b2afe97d26a7cc6d1f92c82519d552966e2af54
Matti <mjsillan@iki.fi>
2018-12-07 14:57:31 +0200

    Add submission for assignment 4.6.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,26 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const mostBlogs = (blogs) => {
+  const blogCounts = {}
+  blogs.forEach(b => {
+    if (!blogCounts[b.author]) {
+      blogCounts[b.author] = { author: b.author, blogs: 0 }
+    }
+    blogCounts[b.author].blogs++
+  })
+
+  return Object.entries(blogCounts).reduce((prev, current) => {
+    if (current[1].blogs > prev.blogs) {
+      return current[1]
+    } else {
+      return prev
+    }
+  }, { blogs: -1 })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  mostBlogs
 }
